61b4216ebad646408219047e8d2e318ef6813b83
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-06 22:56:29 -0500

    Update BertNERTokenizer.py

--- BertNERTokenizer.py
+++ BertNERTokenizer.py
@@ -1,1 +1,8 @@
-
+import tor
+class BertNERTokenizer(torch.nn.Module):
+    def __init__(self, ner_config):
+        super().__init__()
+        self.ner_config
+        
+    def batch_encode_plus(self, 
+    
