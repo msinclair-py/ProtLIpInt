8fbfe85c7b18132307ca84713f2f5933473b80e6
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-14 18:07:52 -0500

    Update train.py

--- train.py
+++ train.py
@@ -38,10 +38,10 @@ def get_args():
 
     #Optimizer related
     parser.add_argument('--max-epochs', default=60, type=int, help='number of epochs max')
-    parser.add_argument('--min-epochs', default=1, type=int, help='number of epochs min')
+    parser.add_argument('--min-epochs', default=-1, type=int, help='number of epochs min')
     parser.add_argument('--batch-size', '-b', default=2048, type=int, help='batch size')
     parser.add_argument('--learning-rate', '-lr', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('--ngpus', type=int, default=-1, help='Number of GPUs, -1 use all available. Use CUDA_VISIBLE_DEVICES=1, to decide gpus')
+    parser.add_argument('--ngpus', type=int, default=1, help='Number of GPUs, -1 use all available. Use CUDA_VISIBLE_DEVICES=1, to decide gpus')
     parser.add_argument('--num-nodes', type=int, default=1, help='Number of nodes')
     parser.add_argument('--warm-up-split', type=int, default=5, help='warmup times')
     parser.add_argument('--scheduler', type=str, default="cosine", help='scheduler type')
@@ -129,6 +129,7 @@ def _main():
     #tb_logger = pl.loggers.TensorBoardLogger("tb_logs", name="my_model")
 
     trainer = pl.Trainer(
+    accelerator="ddp",
     gpus=hparams.ngpus,
     max_epochs=hparams.max_epochs,
     min_epochs=hparams.min_epochs,
