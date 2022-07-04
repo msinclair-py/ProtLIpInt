69a2b981e8914739b24aa96daf932831f5a181e8
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-04 15:42:00 -0500

    Update BertNERTokenizer.py

--- BertNERTokenizer.py
+++ BertNERTokenizer.py
@@ -315,6 +315,7 @@ if __name__ == "__main__":
     for seg in segs:
         end_idx_p1 = np.sum(all_segnames == seg) + start_idx
         current_slice = all_resnames[slice(start_idx, end_idx_p1)] + ["[SEP]"]
+        print(current_slice)
         modified_slice += current_slice
         start_idx = end_idx_p1
     modified_slice.pop() #last SEP token should be gone! #<SEQ1 + SEP + SEQ2 + SEP + SEQ3 ...>
