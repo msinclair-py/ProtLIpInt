55a69f2a034e280b449e7abd7b5ddab73ec9349e
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-12 14:53:23 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -133,7 +133,8 @@ class SequenceDataset(torch.utils.data.Dataset):
         potential_files = os.listdir(directory)
         filtered_files = list(filter(lambda inp: os.path.splitext(inp)[1] == ".json", potential_files))
         resnum_list = SequenceDataset.residue_length_check(filtered_files)
-        max_residue = max(resnum_list)
+#         max_residue = max(resnum_list)
+        max_residue = 300
         hparams.max_residue: int = max_residue #set a new attribute for Argparser; maximum residue num across json files!
 #         print(hparams.max_residue)
 #         hparams.resnum_list_idx: List[int] = np.arange(len(resnum_list)) #set a new attribute for Argparser
