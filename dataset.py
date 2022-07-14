9ebf96935f4cc2498309e309ab6d0d3a88967d59
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-14 11:47:31 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -110,7 +110,7 @@ class SequenceDataset(torch.utils.data.Dataset):
 #         print(len(all_resnames))
         all_resnames = all_resnames + (max_residue - len(all_resnames)) * ["[PAD]"] #WIP
 #         print((max_residue - len(seq)))
-        print(cf.red(max_residue, len(all_resnames) == len(seq)))
+        print(cf.red(f"Max num residues: {max_residue}, No duplicates: {len(all_resnames) == len(seq)}"))
         
         assert np.isin(all_segnames, segs).all(), "all segnames must match..."
         start_idx = 0
