01e0f8e3aa1a8aeef31bf036f22ab69e09a1e6d3
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-07-26 16:48:00 -0500

    Update model.py

--- model.py
+++ model.py
@@ -398,7 +398,7 @@ class ProtBertClassifier(pl.LightningModule):
         Returns:
             - Dictionary with metrics to be added to the lightning logger.  
         """
-        if not self.trainer.running_sanity_check:
+        if not self.trainer.sanity_checking:
             val_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
             val_acc_mean = torch.stack([x['val_acc'] for x in outputs]).mean()
             val_ham_mean = torch.stack([x['val_ham'] for x in outputs]).mean()
